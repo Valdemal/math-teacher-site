fd1fb14c7a72257762614b2011f2ce0d134df97e
Vladimir Nekhaenko <vovanex12@gmail.com>
2024-03-29 18:45:27 +0300

    Сверстал раздел "главное"

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,13 +1,39 @@
 <template>
   <div class="container">
-    Главная информация
+    <div class="text-container">
+      <p>Учитель математики в Борисовской СОШ №1</p>
+      <h2>
+        Семенова<br/>
+        Татьяна<br/>
+        Даниловна
+      </h2>
+    </div>
+    <portrait-component/>
   </div>
 </template>
 <style scoped>
 .container {
   height: 900px;
   background-color: #F1EFF0;
+  display: flex;
 }
+
+.text-container {
+  margin: auto auto auto 120px;
+}
+
+h2 {
+  font-size: 96px;
+  margin-top: 0;
+}
+
+p {
+  color: #68A47F;
+  font: italic 40px VivaldiD, sans-serif;
+  margin-bottom: 5px;
+}
+
 </style>
 <script setup>
+import PortraitComponent from "@/components/PortraitComponent.vue";
 </script>
---
+++ src/components/PortraitComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="portrait-container">
+    <img src="../assets/images/portrait.jpg" alt="Семёнова Татьяна Даниловна">
+  </div>
+</template>
+
+<style scoped>
+.portrait-container {
+  overflow: hidden;
+}
+
+.portrait-container img {
+  transform: translate(0, -75px);
+}
+</style>
